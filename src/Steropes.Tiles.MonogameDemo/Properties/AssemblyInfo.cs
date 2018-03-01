b213181768c6d3e1bc82492ba410e013c569b3fd
Thomas Morgner <tmorgner@tmorgner.com>
2018-03-01 16:35:40 +0000

    Updating version info for staging branch 1.0.0-beta.1

--- src/Steropes.Tiles.MonogameDemo/Properties/AssemblyInfo.cs
+++ src/Steropes.Tiles.MonogameDemo/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -31,6 +31,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+// [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: AssemblyInformationalVersion("1.0.0-beta.1+0.Branch.release-1.0.0.Sha.c3a3e493e311ebb4aba355121346e85ea8d43e9c")]
